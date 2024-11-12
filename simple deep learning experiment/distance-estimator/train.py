83c0d77d704a4a2f03b112b20ce4a70ee519a15a
Street Coding <oldmanstreetcoding@gmail.com>
2024-11-12 07:48:33 -0500

    re-run hyperparam with capturing log process

--- simple deep learning experiment/distance-estimator/train.py
+++ simple deep learning experiment/distance-estimator/train.py
@@ -1,15 +1,84 @@
+# import pandas as pd
+# import numpy as np
+# import time
+
+# from keras.models import Sequential
+# from keras.layers import Dense
+# from sklearn.preprocessing import StandardScaler
+# from keras.callbacks import EarlyStopping, ReduceLROnPlateau, TensorBoard
+# from keras.optimizers import Adam  # Use Adam optimizer as found in hyperparameter search
+
+# def main():
+#     # ----------- import data and scaling ----------- #
+#     df_train = pd.read_csv('data/train.csv')
+#     df_test = pd.read_csv('data/test.csv')
+
+#     X_train = df_train[['xmin', 'ymin', 'xmax', 'ymax']].values
+#     y_train = df_train[['zloc']].values
+
+#     X_test = df_test[['xmin', 'ymin', 'xmax', 'ymax']].values
+#     y_test = df_test[['zloc']].values
+
+#     # standardized data with separate scalers
+#     X_scaler = StandardScaler()
+#     y_scaler = StandardScaler()
+
+#     X_train = X_scaler.fit_transform(X_train)
+#     y_train = y_scaler.fit_transform(y_train)
+
+#     # ----------- create model with best hyperparameters ----------- #
+#     model = Sequential()
+#     model.add(Dense(64, input_dim=4, kernel_initializer='normal', activation='relu'))  # First hidden layer with 64 units
+#     model.add(Dense(32, kernel_initializer='normal', activation='relu'))               # Second hidden layer with 32 units
+#     model.add(Dense(32, kernel_initializer='normal', activation='relu'))               # Third hidden layer with 32 units (from 'Dense_2': 'three')
+#     model.add(Dense(1, kernel_initializer='normal'))                                   # Output layer
+
+#     # Compile model using Adam optimizer
+#     model.compile(loss='mean_squared_error', optimizer=Adam(), metrics=['mae'])
+
+#     # ----------- define callbacks ----------- #
+#     earlyStopping = EarlyStopping(monitor='val_loss', patience=20, verbose=1, mode='min')
+#     reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10,
+#                                        verbose=1, min_delta=1e-5, mode='min')
+#     modelname = "model@{}".format(int(time.time()))
+#     tensorboard = TensorBoard(log_dir="logs/{}".format(modelname))
+
+#     # ----------- start training with optimal batch size ----------- #
+#     history = model.fit(X_train, y_train,
+#                         validation_split=0.1, epochs=5000, batch_size=1024,  # Optimal batch size
+#                         callbacks=[tensorboard, earlyStopping, reduce_lr_loss], verbose=1)
+
+#     # ----------- save model and weights ----------- #
+#     model_json = model.to_json()
+#     with open("generated_files/{}.json".format(modelname), "w") as json_file:
+#         json_file.write(model_json)
+
+#     model.save_weights("generated_files/{}.weights.h5".format(modelname))
+#     print("Saved model to disk")
+
+# if __name__ == '__main__':
+#     main()
+
 import pandas as pd
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+import csv
 
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, TensorBoard
-from keras.optimizers import Adam  # Use Adam optimizer as found in hyperparameter search
+from keras.optimizers import Adam
+from keras.optimizers import RMSprop  # Import RMSprop optimizer
+
+def within_5_percent(y_true, y_pred):
+    margin = 0.05
+    within_margin = np.abs((y_pred - y_true) / y_true) <= margin
+    return np.mean(within_margin)
 
 def main():
-    # ----------- import data and scaling ----------- #
+    # ----------- Import data and perform scaling ----------- #
     df_train = pd.read_csv('data/train.csv')
     df_test = pd.read_csv('data/test.csv')
 
@@ -19,36 +88,61 @@ def main():
     X_test = df_test[['xmin', 'ymin', 'xmax', 'ymax']].values
     y_test = df_test[['zloc']].values
 
-    # standardized data with separate scalers
+    # Standardize data with separate scalers
     X_scaler = StandardScaler()
     y_scaler = StandardScaler()
 
     X_train = X_scaler.fit_transform(X_train)
     y_train = y_scaler.fit_transform(y_train)
 
-    # ----------- create model with best hyperparameters ----------- #
+    X_test = X_scaler.transform(X_test)
+    y_test = y_scaler.transform(y_test)
+
+    # ----------- Create model with best hyperparameters ----------- #
+    # Updated hyperparameters based on tuning results
+    layer_1_units = 32
+    layer_2_units = 64
+    layer_3_units = 16
+    batch_size = 1024
+    optimizer = Adam()
+
+    # layer_1_units = 128
+    # layer_2_units = 64
+    # layer_3_units = 32
+    # batch_size = 512
+    # optimizer = RMSprop()
+
     model = Sequential()
-    model.add(Dense(64, input_dim=4, kernel_initializer='normal', activation='relu'))  # First hidden layer with 64 units
-    model.add(Dense(32, kernel_initializer='normal', activation='relu'))               # Second hidden layer with 32 units
-    model.add(Dense(32, kernel_initializer='normal', activation='relu'))               # Third hidden layer with 32 units (from 'Dense_2': 'three')
-    model.add(Dense(1, kernel_initializer='normal'))                                   # Output layer
+    model.add(Dense(layer_1_units, input_dim=4, activation='relu'))  # First hidden layer
+    model.add(Dense(layer_2_units, activation='relu'))               # Second hidden layer
+    model.add(Dense(layer_3_units, activation='relu'))               # Third hidden layer
+
+    # # Check if the third layer is required (according to the hyperparameters result)
+    # if layer_3_units is not None:
+    #     model.add(Dense(layer_3_units, activation='relu'))
+
+    # model.add(Dense(1, kernel_initializer='normal')) # Output layer
+    model.add(Dense(1)) # Output layer
 
     # Compile model using Adam optimizer
-    model.compile(loss='mean_squared_error', optimizer=Adam(), metrics=['mae'])
+    model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mae'])
 
-    # ----------- define callbacks ----------- #
+    # ----------- Define callbacks ----------- #
     earlyStopping = EarlyStopping(monitor='val_loss', patience=20, verbose=1, mode='min')
     reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10,
                                        verbose=1, min_delta=1e-5, mode='min')
     modelname = "model@{}".format(int(time.time()))
     tensorboard = TensorBoard(log_dir="logs/{}".format(modelname))
 
-    # ----------- start training with optimal batch size ----------- #
+    # ----------- Start training with optimal batch size ----------- #
     history = model.fit(X_train, y_train,
-                        validation_split=0.1, epochs=5000, batch_size=1024,  # Optimal batch size
-                        callbacks=[tensorboard, earlyStopping, reduce_lr_loss], verbose=1)
+                        validation_split=0.1,
+                        epochs=5000,
+                        batch_size=batch_size,
+                        callbacks=[tensorboard, earlyStopping, reduce_lr_loss],
+                        verbose=1)
 
-    # ----------- save model and weights ----------- #
+    # ----------- Save model and weights ----------- #
     model_json = model.to_json()
     with open("generated_files/{}.json".format(modelname), "w") as json_file:
         json_file.write(model_json)
@@ -56,5 +150,81 @@ def main():
     model.save_weights("generated_files/{}.weights.h5".format(modelname))
     print("Saved model to disk")
 
+    # ----------- Save loss and metrics to CSV ----------- #
+    # Save training and validation loss for each epoch
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    mae = history.history['mae']
+    val_mae = history.history['val_mae']
+    epochs = range(1, len(loss) + 1)
+
+    # Save to CSV
+    with open('generated_files/{}_training_log_5.csv'.format(modelname), 'w', newline='') as csvfile:
+        fieldnames = ['epoch', 'loss', 'val_loss', 'mae', 'val_mae']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(len(epochs)):
+            writer.writerow({
+                'epoch': epochs[i],
+                'loss': loss[i],
+                'val_loss': val_loss[i],
+                'mae': mae[i],
+                'val_mae': val_mae[i]
+            })
+
+    # ----------- Visualize training ----------- #
+    plt.figure(figsize=(12, 6))
+    plt.plot(epochs, loss, label='Training Loss')
+    plt.plot(epochs, val_loss, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+    plt.savefig('generated_files/{}_loss_plot_5.png'.format(modelname))
+    plt.show()
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(epochs, mae, label='Training MAE')
+    plt.plot(epochs, val_mae, label='Validation MAE')
+    plt.xlabel('Epochs')
+    plt.ylabel('Mean Absolute Error')
+    plt.title('Training and Validation MAE')
+    plt.legend()
+    plt.savefig('generated_files/{}_mae_plot_5.png'.format(modelname))
+    plt.show()
+
+    # ----------- Evaluate on test data ----------- #
+    test_loss, test_mae = model.evaluate(X_test, y_test, verbose=1)
+    print('Test Loss:', test_loss)
+    print('Test MAE:', test_mae)
+
+    # ----------- Calculate 5% margin accuracy ----------- #
+    y_pred = model.predict(X_test)
+    y_test_inv = y_scaler.inverse_transform(y_test)
+    y_pred_inv = y_scaler.inverse_transform(y_pred)
+    five_percent_accuracy = within_5_percent(y_test_inv, y_pred_inv) * 100
+    print('Percentage of predictions within 5% margin:', five_percent_accuracy, '%')
+
+    # ----------- Save configuration and metrics to CSV ----------- #
+    with open('generated_files/{}_final_results_5.csv'.format(modelname), 'w', newline='') as csvfile:
+        fieldnames = ['Hidden Layer 1', 'Hidden Layer 2', 'Hidden Layer 3', 
+                      'Batch Size', 'Optimizer', 'Validation MSE', 'Validation MAE', 
+                      '5% Margin Accuracy', 'Loss History', 'Validation Loss History']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({
+            'Hidden Layer 1': layer_1_units,
+            'Hidden Layer 2': layer_2_units,
+            'Hidden Layer 3': layer_3_units,
+            'Batch Size': batch_size,
+            'Optimizer': optimizer,
+            'Validation MSE': test_loss,
+            'Validation MAE': test_mae,
+            '5% Margin Accuracy': five_percent_accuracy,
+            'Loss History': ','.join(map(str, loss)),
+            'Validation Loss History': ','.join(map(str, val_loss))
+        })
+
 if __name__ == '__main__':
     main()
+
--- simple deep learning experiment/prediction-visualizer.py
+++ simple deep learning experiment/prediction-visualizer.py
@@ -1,12 +1,58 @@
+# '''
+# Purpose: visualize data from the dataframe
+# '''
+# import os
+# import cv2
+# import pandas as pd
+
+# # Load the prediction results
+# df = pd.read_csv("distance-estimator/data/predictions_1731390048.csv")
+
+# for idx, row in df.iterrows():
+#     if os.path.exists(os.path.join("original_data/train_annots/", row['filename'])):
+#         # Read the image
+#         fp = os.path.join("original_data/train_images", row['filename'].replace('.txt', '.png'))
+#         im = cv2.imread(fp)
+
+#         # Get bounding box coordinates
+#         x1 = int(row['xmin'])
+#         y1 = int(row['ymin'])
+#         x2 = int(row['xmax'])
+#         y2 = int(row['ymax'])
+
+#         # Draw a center line (optional visual aid)
+#         cv2.line(im, (int(1224/2), 0), (int(1224/2), 370), (255, 255, 255), 2)
+#         # Draw the bounding box around the detected object
+#         cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+#         # Calculate the percentage error between actual and predicted zloc
+#         actual = row['zloc']
+#         predicted = row['zloc_pred']
+#         percentage_error = abs(actual - predicted) / actual * 100 if actual != 0 else 0
+
+#         # Prepare the text to display
+#         info_string = "(act: {:.2f}, pred: {:.2f}, err: {:.2f}%)".format(actual, predicted, percentage_error)
+
+#         # Display the information at the center of the bounding box
+#         cv2.putText(im, info_string, (int((x1 + x2) / 2), int((y1 + y2) / 2)),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+
+#         # Show the image with the bounding box and additional information
+#         cv2.imshow("detections", im)
+#         cv2.waitKey(0)
+
+# # Close all windows
+# cv2.destroyAllWindows()
+
 '''
-Purpose: visualize data from the dataframe
+Purpose: visualize data from the dataframe with 5% margin consideration
 '''
 import os
 import cv2
 import pandas as pd
 
 # Load the prediction results
-df = pd.read_csv("distance-estimator/data/predictions7.csv")
+df = pd.read_csv("distance-estimator/data/predictions_1731390048.csv")
 
 for idx, row in df.iterrows():
     if os.path.exists(os.path.join("original_data/train_annots/", row['filename'])):
@@ -22,20 +68,30 @@ for idx, row in df.iterrows():
 
         # Draw a center line (optional visual aid)
         cv2.line(im, (int(1224/2), 0), (int(1224/2), 370), (255, 255, 255), 2)
-        # Draw the bounding box around the detected object
-        cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 3)
-
+        
         # Calculate the percentage error between actual and predicted zloc
         actual = row['zloc']
         predicted = row['zloc_pred']
         percentage_error = abs(actual - predicted) / actual * 100 if actual != 0 else 0
+        
+        # Check if the error is within 5% margin
+        within_5_percent = percentage_error <= 5
+
+        # Choose color for the bounding box based on 5% margin
+        bbox_color = (0, 255, 0) if within_5_percent else (0, 0, 255)  # Green if within 5%, else red
+
+        # Draw the bounding box around the detected object
+        cv2.rectangle(im, (x1, y1), (x2, y2), bbox_color, 3)
 
         # Prepare the text to display
-        info_string = "(act: {:.2f}, pred: {:.2f}, err: {:.2f}%)".format(actual, predicted, percentage_error)
+        margin_text = "within 5%" if within_5_percent else "outside 5%"
+        info_string = "(act: {:.2f}, pred: {:.2f}, err: {:.2f}%, {})".format(
+            actual, predicted, percentage_error, margin_text
+        )
 
         # Display the information at the center of the bounding box
         cv2.putText(im, info_string, (int((x1 + x2) / 2), int((y1 + y2) / 2)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, bbox_color, 2, cv2.LINE_AA)
 
         # Show the image with the bounding box and additional information
         cv2.imshow("detections", im)
@@ -43,3 +99,4 @@ for idx, row in df.iterrows():
 
 # Close all windows
 cv2.destroyAllWindows()
+
